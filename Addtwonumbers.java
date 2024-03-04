5245976378eaff1b947f200f68afc138bda92055
Ananya-a4 <162185893+Ananya-a4@users.noreply.github.com>
2024-03-05 00:59:12 +0530

    Create Addtwonumbers.java

---
+++ Addtwonumbers.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Sum of these no. are");
+        int num1 = 5;
+        int num2 = 8;
+        int num3 = 4;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
+
+
